8aed295ebbfcfc5d4fc5736774df91d384e0d76b
turfanda <irfannturk@gmail.com>
2018-07-15 13:21:02 +0300

    select with attribute and class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './practice1/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './practice1/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
