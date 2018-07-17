662f54ddf0164c7df500b87d6d8ae0e1949859f4
İrfan Türk <irfannturk@gmail.com>
2018-07-17 10:10:27 +0300

    star recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { WarningAlertComponent } from './practice1/warning-alert/warning-alert.c
 import { SuccessAlertComponent } from './practice1/success-alert/success-alert.component';
 import { Practice2Component } from './practice2/practice2.component';
 import { Practice3Component } from './practice3/practice3.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { Practice3Component } from './practice3/practice3.component';
     WarningAlertComponent,
     SuccessAlertComponent,
     Practice2Component,
-    Practice3Component
+    Practice3Component,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
